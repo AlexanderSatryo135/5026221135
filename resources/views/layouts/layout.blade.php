ed60db92c3e1a74d9f44733c2ef888c24a61f16a
AlexanderSatryo135 <dhitoalex123@gmail.com>
2024-12-02 13:51:26 +0700

    Tugas 2 Desember 2024 (kertashvs)

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Alexander Satryo - 5026221135</title>
+    <title>Alexander Satryo Pinandhito Yudho Pratomo - 5026221135</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Aplikasi Laravel')</title>
@@ -16,7 +16,7 @@
     </nav>
 
     <div class="container">
-        <h2>Alexander Satryo - 5026221135</h2>
+        <h2>Alexander Satryo Pinandhito Yudho Pratomo - 5026221135</h2>
         @yield('content')
     </div>
 
